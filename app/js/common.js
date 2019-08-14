97c463e3296872d463a2470445ee3d1f86806b3a
ViktoriaZaharova <zaharova181114@gmail.com>
2019-08-14 16:23:47 +0300

    add css

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+    $('.dropdown').hover(function () {
+        $('.overlay').fadeToggle();
+        $(this).toggleClass('on');
+    });
+
+    $('.block-search > form').click(function () {
+        $(this).siblings('.block-search__dropdown').fadeIn();
+    });
+
+    $('.drop-close').click(function () {
+        $('.block-search__dropdown').fadeOut();
+    });
+
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $(".block-search__dropdown"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.fadeOut(); // скрываем его
+        }
+    });
+
+
+    $('.top-banner__content .btn-close').click(function (e) {
+        e.preventDefault();
+       $('.top-banner').fadeOut();
+    });
 });
